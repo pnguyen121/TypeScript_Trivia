894915ce650638c25d8f744644265383ddafff0c
pnguyen121 <phillynguyen17@gmail.com>
2023-04-03 11:50:39 -0400

    addedd front from google, and got open trivia api link saved

--- Typescript_Trivia/src/App.tsx
+++ Typescript_Trivia/src/App.tsx
@@ -3,10 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+// API LINK HERE TO SAVE https://opentdb.com/api.php?amount=10&category=15&type=multiple
+
 function App() {
   return (
-    <div className="container max-w-full h-screen text-white bg-[url('https://imgur.com/8csQzXk.png')]" id='main-container'>
-      Trivia
+    <div className="container max-w-full h-screen text-white bg-[url('https://imgur.com/N6abCOy.png')]" id='main-container'>
+      Trivia COOLKIDS, NIKE 
     </div>
   )
 }
