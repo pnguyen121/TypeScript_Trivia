e25f3556ea3f405736659f09d1a959fc5237af5e
pnguyen121 <phillynguyen17@gmail.com>
2023-04-09 23:43:54 -0400

    set up ternary statement for the next question button to appear

--- Typescript_Trivia/src/App.tsx
+++ Typescript_Trivia/src/App.tsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 // import function from API.ts file and Types named difficulty
-import { Difficulty, fetchQuizQuestions, QuestionState} from './API'
+import { Difficulty, fetchQuizQuestions, QuestionState } from "./API";
 
-import QuestionCard from './Components/QuestionCard'
-
+import QuestionCard from "./Components/QuestionCard";
 
 // Type answer object
 type AnswerObject = {
@@ -15,113 +14,101 @@ type AnswerObject = {
   answer: string;
   correct: boolean;
   correctAnswer: string;
-
-}
-
+};
 
 // API LINK HERE TO SAVE https://opentdb.com/api.php?amount=10&category=15&type=multiple
-
 
 // Set amount of questions that we can change later if we want
 const TOTAL_QUESTIONS = 10;
 
-
-
 function App() {
-
   // States
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
   // letting it know the array will be from Question state so if anything other than that it will error
-  const [questions, setQuestions] = useState<QuestionState[]>([])
-  const [number, setNumber] = useState(0)
-  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
-  const [score, setScore] = useState(0)
-  const [gameOver, setGameOver] = useState(true)
-
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
 
   // CONSOLE.LOG QUIZ QUESTIONS TO SEE in browsser console, for testing early on
   // console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY), "fetch quiz qs on app.tsx")
 
-
-// START TRIVIA BY RUNNING THIS | Connect it to onClick for the button in the HTML
+  // START TRIVIA BY RUNNING THIS | Connect it to onClick for the button in the HTML
   const startTrivia = async () => {
     // set loading to true
-    setLoading(true)
+    setLoading(true);
     // its not game over so its false
-    setGameOver(false)
+    setGameOver(false);
 
     try {
       // Grab data from fetchQuizQuestions
-    const newQuestions = await fetchQuizQuestions(
-      // # of questions
-      TOTAL_QUESTIONS,
-      // Difficulty
-      Difficulty.EASY
-    );
+      const newQuestions = await fetchQuizQuestions(
+        // # of questions
+        TOTAL_QUESTIONS,
+        // Difficulty
+        Difficulty.EASY
+      );
 
-    // Set the questions to the data we grabbed from the await
-    setQuestions(newQuestions)
-    setScore(0)
-    setUserAnswers([])
-    setNumber(0)
-    // done loading
-    setLoading(false)
-
+      // Set the questions to the data we grabbed from the await
+      setQuestions(newQuestions);
+      setScore(0);
+      setUserAnswers([]);
+      setNumber(0);
+      // done loading
+      setLoading(false);
     } catch (error) {
-      console.log(error, 'Error in App.tsx startTrivia funct')
-      
+      console.log(error, "Error in App.tsx startTrivia funct");
     }
-
-  }
-
+  };
 
   // Tell it the type by writing React.MouseEvent and let it knows its an HTML button that will be clicked
-  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
 
-  } 
-
-
-  const nextQuestion = () => {
-
-
-  }
-
-
+  const nextQuestion = () => {};
 
   return (
-    <div className="container max-w-full h-screen text-white bg-[url('https://imgur.com/N6abCOy.png')]" id='main-container'>
+    <div
+      className="container max-w-full h-screen text-white bg-[url('https://imgur.com/N6abCOy.png')]"
+      id="main-container"
+    >
       <h1>Trivia</h1>
       {/* ternary to get rid of start button after its hit */}
       {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
-        <button className='btn btn-stone' onClick={startTrivia}>Start Game</button>
-      ): <p>change this to null TEST</p> }
+        <button className="btn btn-stone" onClick={startTrivia}>
+          Start Game
+        </button>
+      ) : (
+        <p>change this to null TEST</p>
+      )}
 
-
-        {/* If gameover = false (not game over) show score while game isnt over*/}
-      {!gameOver ? <p className=''>Score:</p> : null}
-      {loading ? <p className=''>Loading Questions ...</p> : null}
+      {/* If gameover = false (not game over) show score while game isnt over*/}
+      {!gameOver ? <p className="">Score:</p> : null}
+      {/* if loading show a p like below */}
+      {loading ? <p className="">Loading Questions ...</p> : null}
 
       {!loading && !gameOver && (
-      <QuestionCard 
-      questionNr={number + 1}
-      totalQuestions={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-      />
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestions={TOTAL_QUESTIONS}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          callback={checkAnswer}
+        />
       )}
-      {/* <QuestionCard 
-      questionNr={number + 1}
-      totalQuestions={TOTAL_QUESTIONS}
-      question={questions[number].question}
-      answers={questions[number].answers}
-      userAnswer={userAnswers ? userAnswers[number] : undefined}
-      callback={checkAnswer}
-      /> */}
-      <button className='btn btn-stone' onClick={nextQuestion}>Next Question</button>
+
+{/* Only show the next question button if game isnt over, isnt loading, and userAnswerss is === etc etc */}
+      {!gameOver &&
+      !loading &&
+      userAnswers.length === number + 1 &&
+      number !== TOTAL_QUESTIONS - 1 ? (
+        <button className="btn btn-stone" onClick={nextQuestion}>
+          Next Question
+        </button>
+      ) : null}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
