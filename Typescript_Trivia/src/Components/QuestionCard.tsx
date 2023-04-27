19476c54b742e2552a22d5ddb616cb8fed04aa01
pnguyen121 <phillynguyen17@gmail.com>
2023-04-27 16:05:07 -0400

    button styling back to normal still working on incorrect answers

--- Typescript_Trivia/src/Components/QuestionCard.tsx
+++ Typescript_Trivia/src/Components/QuestionCard.tsx
@@ -42,7 +42,7 @@ function QuestionCard({
         {answers.map((answer) => (
           <div key={answer} correct-ans={userAnswer?.correctAnswer === answer} user-Clicked={userAnswer?.answer === answer}>
             <button
-              className={userAnswer?.correctAnswer == answer ? 'bg-green-500' : 'btn'} 
+              className={userAnswer?.correctAnswer == answer ? 'bg-green-500' : 'btn-2'} 
             //   {userAnswer?.answer == answer ? 'btn-2' : 'bg-red-500'}
               disabled={userAnswer ? true : false}
               value={answer}
